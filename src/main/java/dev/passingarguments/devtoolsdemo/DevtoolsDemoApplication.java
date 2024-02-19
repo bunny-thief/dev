ae92ca104ead349a480d74d77b92876c5e132c4d
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-02-19 11:03:29 +0100

    Initial commit

---
+++ src/main/java/dev/passingarguments/devtoolsdemo/DevtoolsDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.devtoolsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtoolsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtoolsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/passingarguments/devtoolsdemo/DevtoolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.devtoolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtoolsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
